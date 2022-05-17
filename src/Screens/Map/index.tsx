5bfba75cb555b98b024d42a31e735682fb7565f2
yash.sahu <yash.sahu@app-scoop.com>
2022-05-17 17:38:49 +0530

    added dynamic location cards

--- src/Screens/Map/index.tsx
+++ src/Screens/Map/index.tsx
@@ -1,60 +1,200 @@
-import { View } from 'react-native';
-import React, { useEffect, useState } from 'react';
+import { Text, View, Animated, Dimensions, Platform } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import styles from './styles';
-import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps';
 import Statusbar from '../../Components/Statusbar';
+
+const { width } = Dimensions.get('window');
+const CARD_WIDTH = width * 0.8;
 
 const Map = () => {
     const records = useSelector(state => state.realm.records);
     const [parsedRecords, setParsedRecords] = useState([]);
+    const _map = useRef(null);
+    const _scrollView = useRef(null);
+
+    let mapIndex = 0;
+    let mapAnimation = new Animated.Value(0);
 
     useEffect(() => {
         const _parsedRecords = records.map(item => {
+            const { name, phone } = item;
             const address = JSON.parse(item?.address);
             return {
+                name,
+                phone,
                 latitude: address?.latitude,
                 longitude: address?.longitude,
-                place: address?.display_name,
+                place: address?.address?.display_name,
             };
         });
         setParsedRecords(_parsedRecords);
     }, []);
 
-    return (
+    // snap effect
+    useEffect(() => {
+        mapAnimation.addListener(({ value }) => {
+            // animate 30% away from landing on the next item
+            let index = Math.floor(value / CARD_WIDTH + 0.3);
+            if (index >= parsedRecords.length) {
+                index = parsedRecords.length - 1;
+            }
+            if (index <= 0) {
+                index = 0;
+            }
+
+            const regionTimeout = setTimeout(() => {
+                if (mapIndex !== index) {
+                    mapIndex = index;
+                    const { latitude, longitude } = parsedRecords[index];
+                    _map.current.animateToRegion(
+                        {
+                            latitude,
+                            longitude,
+                            latitudeDelta: 0.04864195044303443,
+                            longitudeDelta: 0.040142817690068,
+                        },
+                        350,
+                    );
+                }
+            }, 10);
+
+            clearTimeout(regionTimeout);
+        });
+    });
+
+    // scaling animations
+    const interpolations = parsedRecords.map((_, index) => {
+        const inputRange = [
+            (index - 1) * CARD_WIDTH,
+            index * CARD_WIDTH,
+            (index + 1) * CARD_WIDTH,
+        ];
+
+        const scale = mapAnimation.interpolate({
+            inputRange,
+            outputRange: [1, 1.5, 1],
+            extrapolate: 'clamp',
+        });
+        return { scale };
+    });
+
+    // press a marker to show its info
+    const onMarkerPress = e => {
+        const markerId = e._targetInst.return.key;
+
+        let x = markerId * CARD_WIDTH + markerId * 20;
+        if (Platform.OS === 'ios') {
+            x = x - 20;
+        }
+
+        _scrollView.current.scrollTo({ x: x, y: 0, animated: true });
+    };
+
+    return parsedRecords.length > 0 ? (
         <View style={styles.container}>
-            <Statusbar />
-            {parsedRecords.length > 0 && (
-                <MapView
-                    provider={PROVIDER_GOOGLE}
-                    style={styles.map}
-                    initialRegion={{
-                        latitude: parsedRecords[0]?.latitude,
-                        longitude: parsedRecords[0]?.longitude,
-                        latitudeDelta: 0.0922,
-                        longitudeDelta: 0.0421,
-                    }}
-                    showsUserLocation={true}
-                    showsMyLocationButton={true}
-                    followsUserLocation={true}
-                    showsCompass={true}
-                    scrollEnabled={true}
-                    zoomEnabled={true}
-                    pitchEnabled={true}
-                    rotateEnabled={true}>
-                    {parsedRecords.map((item, index) => (
+            <MapView
+                ref={_map}
+                provider={PROVIDER_GOOGLE}
+                style={styles.map}
+                initialRegion={{
+                    latitude: parsedRecords[0]?.latitude,
+                    longitude: parsedRecords[0]?.longitude,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+                showsUserLocation={true}
+                showsMyLocationButton={true}
+                followsUserLocation={true}
+                showsCompass={true}
+                scrollEnabled={true}
+                zoomEnabled={true}
+                pitchEnabled={true}
+                rotateEnabled={true}>
+                {parsedRecords.map((item, index) => {
+                    const scaleStyle = {
+                        transform: [
+                            {
+                                scale: interpolations[index].scale,
+                            },
+                        ],
+                    };
+                    return (
                         <Marker
                             key={index}
-                            title={item?.place}
+                            onPress={e => onMarkerPress(e)}
                             coordinate={{
                                 latitude: item?.latitude,
                                 longitude: item?.longitude,
-                            }}
-                        />
-                    ))}
-                </MapView>
-            )}
+                            }}>
+                            <Animated.View style={[styles.markerWrap]}>
+                                <Animated.Image
+                                    source={require('../../../assets/images/map_marker.png')}
+                                    style={[styles.marker, scaleStyle]}
+                                    resizeMode="cover"
+                                />
+                            </Animated.View>
+                        </Marker>
+                    );
+                })}
+            </MapView>
+
+            {/* Cards */}
+            <Animated.ScrollView
+                ref={_scrollView}
+                horizontal
+                pagingEnabled
+                scrollEventThrottle={1}
+                snapToAlignment="center"
+                snapToInterval={CARD_WIDTH + 20}
+                showsHorizontalScrollIndicator={false}
+                contentInset={{
+                    // for iOS only
+                    top: 0,
+                    bottom: 0,
+                    left: 20,
+                    right: 20,
+                }}
+                contentContainerStyle={{
+                    // for android only
+                    paddingHorizontal: Platform.OS === 'android' ? 20 : 0,
+                }}
+                onScroll={Animated.event(
+                    [
+                        {
+                            nativeEvent: {
+                                contentOffset: {
+                                    x: mapAnimation,
+                                },
+                            },
+                        },
+                    ],
+                    { useNativeDriver: true },
+                )}
+                style={styles.scrollView}>
+                {parsedRecords.map((item, index) => (
+                    <View key={index} style={styles.card}>
+                        <Text style={styles.cardTitle} numberOfLines={1}>
+                            {item?.name}
+                        </Text>
+                        <Text style={styles.cardDesc} numberOfLines={1}>
+                            {item?.phone}
+                        </Text>
+                        <Text style={styles.cardDesc} numberOfLines={1}>
+                            {item?.place}
+                        </Text>
+                    </View>
+                ))}
+            </Animated.ScrollView>
+        </View>
+    ) : (
+        <View style={styles.noRecordsContainer}>
+            <Statusbar />
+            <Text style={styles.noRecordsText}>
+                Please add some records first!
+            </Text>
         </View>
     );
 };
--- src/Screens/Map/styles.ts
+++ src/Screens/Map/styles.ts
@@ -1,23 +1,78 @@
-import { StyleSheet } from 'react-native';
-// import { theme } from '../../utils/theme';
+import { Dimensions, StyleSheet } from 'react-native';
+
+const { width } = Dimensions.get('window');
+const CARD_WIDTH = width * 0.8;
 
 const styles = StyleSheet.create({
-  container: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-  },
+    container: {
+      flex: 1,
+    },
+    map: {
+        height: '100%',
+    },
+    image: {
+        width: 120,
+        height: 80,
+    },
+    scrollView: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        paddingVertical: 10,
+    },
+    card: {
+      padding: 10,
+      elevation: 2,
+      backgroundColor: '#FFF',
+      borderTopLeftRadius: 5,
+      borderTopRightRadius: 5,
+      marginHorizontal: 10,
+      shadowColor: '#000',
+      shadowRadius: 5,
+      shadowOpacity: 0.3,
+      shadowOffset: { x: 2, y: -2 },
+      height: 100,
+      width: CARD_WIDTH,
+      overflow: 'hidden',
+    },
+    cardImage: {
+      flex: 3,
+      width: '100%',
+      height: '100%',
+      alignSelf: 'center',
+    },
+    cardTitle: {
+      color: 'black',
+      fontSize: 18,
+      fontWeight: 'bold',
+    },
+    cardDesc: {
+      fontSize: 16,
+      color: '#444',
+      marginVertical: 2,
+    },
+    markerWrap: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: 50,
+      height: 50,
+    },
+    marker: {
+      width: 30,
+      height: 30,
+    },
+    noRecordsContainer: {
+      flex: 1,
+      padding: 10,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    noRecordsText: {
+      color: 'grey',
+      fontSize: 18,
+      fontWeight: '600',
+    },
 });
 
 export default styles;
